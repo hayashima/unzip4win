cd7c2e706c9f3f80f4201bcef86a27f7b7899e48
Ryo Sumasu <ryo.sms@gmail.com>
2019-02-18 17:06:20 +0900

    Impl unpacking zip sample

--- main.go
+++ main.go
@@ -1,5 +1,50 @@
 package main
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/yeka/zip"
+)
+
 func main() {
-	println("hello")
+	configFile := flag.String("config", "config.toml", "")
+	flag.Parse()
+
+	fmt.Printf("%v\n", flag.Args())
+	fmt.Println(*configFile)
+
+	zipPath := flag.Arg(0)
+	fmt.Println(zipPath)
+
+	reader, err := zip.OpenReader(zipPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer reader.Close()
+
+	for _, f := range reader.File {
+		if f.IsEncrypted() {
+			f.SetPassword("password")
+		}
+		r, err := f.Open()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		buf, err := ioutil.ReadAll(r)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer r.Close()
+
+		fmt.Printf("Size of %v: %v byte(s)\n", f.Name, len(buf))
+	}
+
+	fmt.Print("Input any for exit...")
+	bufio.NewScanner(os.Stdin).Scan()
 }
