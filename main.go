4eb1b9acaff6caa095a719f03eb669729aa8d078
Ryo Sumasu <ryo.sms@gmail.com>
2019-02-19 17:46:34 +0900

    Mod error handling

--- main.go
+++ main.go
@@ -16,12 +16,25 @@ func main() {
 
 	logger, err := unzip4win.AppLogger(args.IsDebug)
 	if err != nil {
-		log.Fatalf("Can't initialize logger: %v", err)
+		log.Printf("Can't initialize logger: %v", err)
+		exitWith(1)
 	}
 
 	logger.Debug("argument is",
 		zap.String("config", args.ConfigFile),
 		zap.String("zip", args.ZipFile))
+
+	config, err := unzip4win.LoadConfig(args.ConfigFile)
+	if err != nil {
+		logger.Info("Can't parse config file.", zap.Error(err))
+		exitWith(1)
+	}
+	logger.Debug(fmt.Sprintf("config: %v", *config))
+
+	if !unzip4win.IsLookLikeZipFile(args.ZipFile) {
+		logger.Info("It seems to be not a zip file.", zap.String("zipPath", args.ZipFile))
+		exitWith(1)
+	}
 
 	reader, err := zip.OpenReader(args.ZipFile)
 	if err != nil {
@@ -47,6 +60,11 @@ func main() {
 		fmt.Printf("Size of %v: %v byte(s)\n", f.Name, len(buf))
 	}
 
+	exitWith(0)
+}
+
+func exitWith(exitCode int) {
 	fmt.Print("Input any for exit...")
 	bufio.NewScanner(os.Stdin).Scan()
+	os.Exit(exitCode)
 }
