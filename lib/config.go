99131a70a9459ab2b5a6c7d0e825d8b236bdaf78
Ryo Sumasu <ryo.sms@gmail.com>
2019-02-19 17:10:45 +0900

    Impl loading config from file

--- lib/config.go
+++ lib/config.go
@@ -1,6 +1,11 @@
 package unzip4win
 
-import "flag"
+import (
+	"flag"
+	"github.com/BurntSushi/toml"
+	"sort"
+	"time"
+)
 
 type Arguments struct {
 	ConfigFile string
@@ -24,4 +29,46 @@ func ParseArgs() *Arguments {
 	}
 
 	return &parsed
+}
+
+type Config struct {
+	Output   OutputConfig
+	Password PasswordConfig
+	Spec     []SpecConfig
+}
+
+type OutputConfig struct {
+	SaveCurrent bool   `toml:"saveCurrent"`
+	OutputPath  string `toml:"outputPath"`
+}
+
+type PasswordConfig struct {
+	TryDays int `toml:"tryDays"`
+}
+
+type SpecConfig struct {
+	Format    string    `toml:"format"`
+	StartDate time.Time `toml:"startDate"`
+}
+
+func LoadConfig(configFile string) (*Config, error) {
+
+	var config Config
+	_, err := toml.DecodeFile(configFile, &config)
+	// order by Spec.StartDate DESC
+	sort.Sort(config)
+
+	return &config, err
+}
+
+func (c Config) Len() int {
+	return len(c.Spec)
+}
+
+func (c Config) Swap(i, j int) {
+	c.Spec[i], c.Spec[j] = c.Spec[j], c.Spec[i]
+}
+
+func (c Config) Less(i, j int) bool {
+	return c.Spec[i].StartDate.Sub(c.Spec[j].StartDate) > 0
 }
