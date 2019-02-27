a0d7ba65b06a5c121b989284178fd085a0c36cd8
Ryo Sumasu <ryo.sms@gmail.com>
2019-02-27 18:14:41 +0900

    Add test for targetSpec

--- lib/unzip_test.go
+++ lib/unzip_test.go
@@ -2,7 +2,9 @@ package unzip4win
 
 import (
 	"path/filepath"
+	"reflect"
 	"testing"
+	"time"
 )
 
 func TestIsLookLikeZipFile(t *testing.T) {
@@ -20,4 +22,48 @@ func TestIsLookLikeZipFile(t *testing.T) {
 			t.Error("expected: false, but actual is true")
 		}
 	})
+}
+
+func TestTargetSpec(t *testing.T) {
+	specs := []SpecConfig{
+		{Format: "pass1", StartDate: createDate(2019, time.January, 1)},
+		{Format: "pass2", StartDate: createDate(2018, time.January, 1)},
+		{Format: "pass3", StartDate: createDate(2017, time.January, 1)},
+	}
+	t.Run("empty spec", func(t *testing.T) {
+		target := createDate(2019, time.April, 1)
+		actual := targetSpec(target, []SpecConfig{})
+		if len(actual) != 0 {
+			t.Errorf("expected size: 0, but actual size is %v\n actual is %v",
+				len(actual), actual)
+		}
+	})
+
+	t.Run("first is matched", func(t *testing.T) {
+		target := createDate(2019, time.April, 1)
+		actual := targetSpec(target, specs)
+		if !reflect.DeepEqual(specs, actual) {
+			t.Errorf("expected is %v, but actutal is %v", specs, actual)
+		}
+	})
+
+	t.Run("first is NOT matched", func(t *testing.T) {
+		target := createDate(2018, time.April, 1)
+		actual := targetSpec(target, specs)
+		if !reflect.DeepEqual(specs[1:], actual) {
+			t.Errorf("expected is %v, but actutal is %v", specs[1:], actual)
+		}
+	})
+
+	t.Run("nothing is matched", func(t *testing.T) {
+		target := createDate(2016, time.December, 31)
+		actual := targetSpec(target, specs)
+		if len(actual) > 0 {
+			t.Errorf("no match is expected, but actutal is %v", actual)
+		}
+	})
+}
+
+func createDate(year int, month time.Month, day int) time.Time {
+	return time.Date(year, month, day, 0, 0, 0, 0, time.Local)
 }
