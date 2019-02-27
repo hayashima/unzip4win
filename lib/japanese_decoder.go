ce1e3e470a56b59599abea89dd1ab863474f008b
Ryo Sumasu <ryo.sms@gmail.com>
2019-02-27 13:54:36 +0900

    Fix handling UTF-8

--- lib/japanese_decoder.go
+++ lib/japanese_decoder.go
@@ -11,6 +11,7 @@ var decoders = map[string]*encoding.Decoder{
 	"Shift_JIS":   japanese.ShiftJIS.NewDecoder(),
 	"EUC-JP":      japanese.EUCJP.NewDecoder(),
 	"ISO-2022-JP": japanese.ISO2022JP.NewDecoder(),
+	"UTF-8":       nil,
 }
 var detector = chardet.NewTextDetector()
 
@@ -24,7 +25,11 @@ func decodeString(original string) (string, error) {
 		decoder, contain := decoders[e.Charset]
 		if contain {
 			debugLog("matched encoding", zap.Any("encoding", e))
-			return decoder.String(original)
+			if decoder == nil {
+				return original, nil
+			} else {
+				return decoder.String(original)
+			}
 		}
 	}
 	return original, nil
